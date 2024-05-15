6ebce6325b371cefebd982bc56b2319fb3938274
Cam's Mac <carrick@Camss-MacBook-Pro.local>
2024-05-15 11:48:16 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Cam's Photos`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
